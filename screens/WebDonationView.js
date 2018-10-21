a965afb4056dc8bd999812935bbcc4d0a64673a5
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2018-10-21 05:06:27 -0500

    minor edit

--- screens/WebDonationView.js
+++ screens/WebDonationView.js
@@ -17,7 +17,7 @@ export default class WebDonationView extends React.Component {
                     document.getElementsByName("last_name")[0].value="???";
                     document.getElementsByName("phone")[0].value="5556667777";
                     document.getElementsByName("farmer_email")[0].value="example@example.com";
-                    document.getElementsByName("comments")[0].value="1000";
+                    document.getElementsByName("comments")[0].value="$1000 of crops";
                     document.getElementsByClassName("select2-chosen")[0].value="IA";
                 `}
                 source={{ uri: 'https://endhunger.org/DB_Registration/Farmers_view.php' }}
